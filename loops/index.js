c3afe93b19c7b4814c688808ff6797467dbbab14
Bruna Pacheco <brunappacheco@hotmail.com>
2020-09-25 17:30:20 -0300

    exercicios de loops no javascript

---
+++ loops/index.js
@@ -0,0 +1,46 @@
+console.log("Oie, eu só sirvo pra saber se as coisas funcionaram!")
+
+//INTERPRETAÇÃO DE CÓDIGO
+//exercicio 1
+// o código esta somando o valor a cada incremnto do i,
+// enquanto ele estiver menor que 5 ( a começar do zero).
+// o resultado seria 10 (valor + i)
+
+
+//exercicio 2
+// a.Serão impressos os numeros da lista que forem maior do que o n 18.
+// b.
+
+//desafio 1
+// Não entendi absolutamente nada!
+
+//ESCRITA DE CÓDIGO
+//exercício 3
+
+//item a.
+const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+for (let i = 0; i < 12; i++){
+  console.log (array [i])
+}
+// *tb poderia usar array.length no lugar do numero 12.
+
+//item b.
+// valores do array original dividido por 10
+
+let numeros = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+for (let i = 0; i < numeros.length; i++){
+  numeros[i] = numeros[i] / 2;
+  console.log(numeros[i]);
+}
+
+//item c.
+// não concluido
+
+//item d.
+// não concluido
+
+//item e.
+// não concluido
+
+
